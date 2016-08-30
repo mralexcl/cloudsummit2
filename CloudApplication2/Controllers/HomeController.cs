0c488d492b26187882c95c8c72b94049002cbada
Alex Campos Arcos <alex_campos_arcos@hotmail.com>
2016-08-30 14:44:46 -0300

    Adding new home

--- CloudApplication2/Controllers/HomeController.cs
+++ CloudApplication2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LaunchDarkly.Client;
 
 namespace CloudApplication2.Controllers
 {
@@ -15,6 +16,23 @@ namespace CloudApplication2.Controllers
 
         public ActionResult About()
         {
+            LdClient ldClient = new LdClient("sdk-0020a10d-45cd-4be2-94fd-87d70780248f");
+
+            User user = LaunchDarkly.Client.User.WithKey(User.Identity.Name);
+            bool showFeature = ldClient.BoolVariation("new-home", user, false);
+            if (showFeature)
+            {
+                // application code to show the feature 
+                return View("NewHome");
+            }
+            else
+            {
+                // the code to run if the feature is off
+                return View();
+            }
+
+
+
             ViewBag.Message = "Your application description page.";
 
             return View();
